f8452398388c5c418c76b9e7ac32890bdda367ee
sampaio23 <sampaio.ime@gmail.com>
2018-04-04 21:58:59 -0300

    main envia os dados pro labview sem enviar corretamente os robos azuis

--- main.cpp
+++ main.cpp
@@ -9,29 +9,12 @@
 #include "cstdlib"
 #include "interface.h"
 
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <errno.h>
-#include <stdlib.h>
 #include <iostream>
-#include <stdio.h>
-
-#include <string.h>
-#include <netinet/in.h>
-#include <stdlib.h>
 #include <sys/socket.h>
-#include <stdio.h>
+#include <arpa/inet.h>
 
-using namespace std;
+#include "viewer.pb.h"
+// #include "labview.pb.h"
 
 Interface interface_receive;                    //! Interface de recebimento de estados
 vss_state::Global_State global_state;           //! Pacote que define um estado
@@ -53,102 +36,60 @@ int main(int argc, char** argv){
 	interface_send.createSendCommandsTeam1(&global_commands, "tcp://localhost:5556");   //! Team2 abre o socket na porta 5557
 	interface_debug.createSendDebugTeam1(&global_debug, "tcp://localhost:5558");       //! Team2 abre o socket na porta 5559
 
-	float s0,s1; // ,s2; //s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16;
 	float* s;
 
-	int server_fd, client;
-	struct sockaddr_in address, addc;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[544];
+    struct sockaddr_in addrF;
+    addrF.sin_family = AF_INET;
+    inet_aton("192.168.0.100", &addrF.sin_addr);
+    addrF.sin_port = htons(9000);
+    int sockF = socket(PF_INET, SOCK_DGRAM, 0);
 
-	if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0){
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+    struct sockaddr_in addrE;
+    addrE.sin_family = AF_INET;
+    inet_aton("192.168.0.100", &addrE.sin_addr);
+    addrE.sin_port = htons(9001);
+    int sockE = socket(PF_INET, SOCK_DGRAM, 0);
 
-	if ((client = socket(AF_INET, SOCK_DGRAM, 0)) == 0){
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    Viewer::DataFriend viewerFriend;
+    Viewer::DataEnemy viewerEnemy;
 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-
-
-	if (setsockopt(client, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(9000);
-
-
-	addc.sin_family = AF_INET;
-	addc.sin_addr.s_addr = inet_addr("192.168.0.101");
-	addc.sin_port = htons(8000);
-
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+    std::string bufFriend;
+    std::string bufEnemy;
 
  	while(true){
 
-		recvfrom(server_fd, buffer, sizeof(buffer), 0, (sockaddr *)&address, (socklen_t *)&addrlen);
-		fflush(stdout);
-
         s = receive_state();
+
+        viewerFriend.set_ballposex(s[0]);
+        viewerFriend.set_ballposey(s[1]);
+        viewerFriend.set_friend1posex(s[2]);
+        viewerFriend.set_friend1posey(s[3]);
+        viewerFriend.set_friend1yaw(s[4]);
+        viewerFriend.set_friend2posex(s[5]);
+        viewerFriend.set_friend2posey(s[6]);
+        viewerFriend.set_friend2yaw(s[7]);
+        viewerFriend.set_friend3posex(s[8]);
+        viewerFriend.set_friend3posey(s[9]);
+        viewerFriend.set_friend3yaw(s[10]);
+
+        viewerEnemy.set_enemy1posex(s[11]);
+        viewerEnemy.set_enemy1posey(s[12]);
+        viewerEnemy.set_enemy2posex(s[13]);
+        viewerEnemy.set_enemy2posey(s[14]);
+        viewerEnemy.set_enemy3posex(s[15]);
+        viewerEnemy.set_enemy3posey(s[16]);
+
+        viewerFriend.SerializeToString(&bufFriend);
+        viewerEnemy.SerializeToString(&bufEnemy);
+
+        sendto(sockF, bufFriend.data(), 65, 0, (struct sockaddr *)&addrF, sizeof(addrF)); // Friend data is sent via port 9000
+        sendto(sockE, bufEnemy.data(), 30, 0, (struct sockaddr *)&addrE, sizeof(addrE)); // Enemy data is sent via port 9001
+
+        bufFriend.clear();
+        bufEnemy.clear();
+
         send_commands();
-        //send_debug();
-
-        s0 = s[0];
-        s1 = s[1];
-        // s2 = s[2];
-        
-        /*s3 = s[3];
-        s4 = s[4];
-        s5 = s[5];
-        s6 = s[6];
-        s7 = s[7];
-        s8 = s[8];
-        s9 = s[9];
-        s10 = s[10];
-        s11 = s[11];
-        s12 = s[12];
-        s13 = s[13];
-        s14 = s[14];
-        s15 = s[15];
-        s16 = s[16];
-*/
-        string resultado0 = to_string(s0);
-        string resultado1 = to_string(s1);
-		// string resultado2 = to_string(s2);
-        /*string resultado3 = to_string(s3);
-        string resultado4 = to_string(s4);
-        string resultado5 = to_string(s5);
-        string resultado6 = to_string(s6);
-        string resultado7 = to_string(s7);
-        string resultado8 = to_string(s8);
-        string resultado9 = to_string(s9);
-        string resultado10 = to_string(s10);
-        string resultado11 = to_string(s11);
-        string resultado12 = to_string(s12);
-        string resultado13 = to_string(s13);
-        string resultado14 = to_string(s14);
-        string resultado15 = to_string(s15);
-        string resultado16 = to_string(s16);
-*/
-
-		string resultado = resultado0+" "+resultado1; //+" "+resultado2; //+" "+resultado3+" "+resultado4+" "+resultado5+" "+resultado6+" "+resultado7+" "+resultado8+" "+resultado9+" "+resultado10+" "+resultado11+" "+resultado12+" "+resultado13+" "+resultado14+" "+resultado15+" "+resultado16;
-        const char* msg = resultado.c_str();
-        size_t msg_length = strlen(msg);
-
-        sendto(client, msg, msg_length, 0, (sockaddr *)&addc, sizeof(addc));
 
 	}       	
 
@@ -160,8 +101,8 @@ float* receive_state(){
     interface_receive.receiveState();           //! Bloqueante até o VSS-Vision ou VSS-Simulator enviar um estado
 
 	float* result;
-	float current[2];
-
+	float current[17];
+/*
 	global_state.balls(0).pose().x();           // Pos X da bola
     global_state.balls(0).pose().y();           // Pos Y da bola
 
@@ -190,28 +131,27 @@ float* receive_state(){
         global_state.robots_blue(i).v_pose().yaw();         // Vel Z do robô azul i
     
     }
-
-
+*/
     current[0] = global_state.balls(0).pose().x();           // Pos X da bola
-    //current[1] = global_state.balls(0).pose().y();           // Pos Y da bola
-	current[1] = global_state.robots_yellow(0).pose().x();           // Pos X do robô amarelo i
-    //current[3] = global_state.robots_yellow(0).pose().y();           // Pos Y do robô amarelo i
-    //current[4] = global_state.robots_yellow(0).pose().yaw();         // Rot Z do robô amarelo i
-    //current[5] = global_state.robots_yellow(1).pose().x();           // Pos X do robô amarelo i
-    //current[6] = global_state.robots_yellow(1).pose().y();           // Pos Y do robô amarelo i
-    //current[7] = global_state.robots_yellow(1).pose().yaw();         // Rot Z do robô amarelo i
-    //current[8] = global_state.robots_yellow(2).pose().x();           // Pos X do robô amarelo i
-    //current[9] = global_state.robots_yellow(2).pose().y();           // Pos Y do robô amarelo i
-    //current[10] = global_state.robots_yellow(2).pose().yaw();         // Rot Z do robô amarelo i
+    current[1] = global_state.balls(0).pose().y();           // Pos Y da bola
+	current[2] = global_state.robots_yellow(0).pose().x();           // Pos X do robô amarelo i
+    current[3] = global_state.robots_yellow(0).pose().y();           // Pos Y do robô amarelo i
+    current[4] = global_state.robots_yellow(0).pose().yaw();         // Rot Z do robô amarelo i
+    current[5] = global_state.robots_yellow(1).pose().x();           // Pos X do robô amarelo i
+    current[6] = global_state.robots_yellow(1).pose().y();           // Pos Y do robô amarelo i
+    current[7] = global_state.robots_yellow(1).pose().yaw();         // Rot Z do robô amarelo i
+    current[8] = global_state.robots_yellow(2).pose().x();           // Pos X do robô amarelo i
+    current[9] = global_state.robots_yellow(2).pose().y();           // Pos Y do robô amarelo i
+    current[10] = global_state.robots_yellow(2).pose().yaw();         // Rot Z do robô amarelo i
 
-    //current[11] = global_state.robots_blue(0).pose().x();             // Pos X do robô azul i
-    //current[12] = global_state.robots_blue(0).pose().y();             // Pos Y do robô azul i
-    //current[13] = global_state.robots_blue(1).pose().x();             // Pos X do robô azul i
-    //current[14] = global_state.robots_blue(1).pose().y();             // Pos Y do robô azul i
-    //current[15] = global_state.robots_blue(2).pose().x();             // Pos X do robô azul i
-    //current[16] = global_state.robots_blue(2).pose().y();             // Pos Y do robô azul i
+    current[11] = global_state.robots_blue(0).pose().x();             // Pos X do robô azul i
+    current[12] = global_state.robots_blue(0).pose().y();             // Pos Y do robô azul i
+    current[13] = global_state.robots_blue(1).pose().x();             // Pos X do robô azul i
+    current[14] = global_state.robots_blue(1).pose().y();             // Pos Y do robô azul i
+    current[15] = global_state.robots_blue(2).pose().x();             // Pos X do robô azul i
+    current[16] = global_state.robots_blue(2).pose().y();             // Pos Y do robô azul i
 
-	result = current; 
+	result = current;
     return result;
 }
 
@@ -222,11 +162,11 @@ void send_commands(){
 	for(int i = 0 ; i < 3 ; i++){                           // todos os robôs vão girar
 		vss_command::Robot_Command *robot = global_commands.add_robot_commands();
 		robot->set_id(i);
-		robot->set_left_vel(15*i);
-		robot->set_right_vel(40*i);
+		robot->set_left_vel(100*i);
+		robot->set_right_vel(50*i);
 		if (i==0){
 			robot->set_left_vel(10);
-			robot->set_right_vel(10);
+			robot->set_right_vel(50);
 		}
 	}
 
